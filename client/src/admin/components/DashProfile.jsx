87a92197c03c497fe06bbe370215a92a598a826c
htung0403 <htung0403@gmail.com>
2024-08-14 22:51:28 +0700

    update user

--- client/src/admin/components/DashProfile.jsx
+++ client/src/admin/components/DashProfile.jsx
@@ -1,64 +1,132 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { Alert, Button, TextInput } from 'flowbite-react'
-import { getDownloadURL, getStorage, uploadBytes, uploadBytesResumable, ref } from 'firebase/storage'
-import { app } from '../../firebase.js'
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { Alert, Button, TextInput } from "flowbite-react";
+import {
+  getDownloadURL,
+  getStorage,
+  uploadBytes,
+  uploadBytesResumable,
+  ref,
+} from "firebase/storage";
+import { app } from "../../firebase.js";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import {
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} from "../../redux/user/userSlice.js";
+import { useDispatch } from "react-redux";
 
 export default function DashProfile() {
-  const {currentUser} = useSelector(state => state.user)
+  const { currentUser } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
-  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null)
-  const [imageFileUploadError, setImageFileUploadError] = useState(null)
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+  const [updateUserError, setUpdateUserError] = useState(null);
+  const [formData, setFormData] = useState({});
   const filePickerRef = useRef();
-  const handleImageChange = (e) =>{
-  const file = e.target.files[0];
-    if (file){
+  const dispatch = useDispatch();
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
       setImageFile(e.target.files[0]);
       setImageFileUrl(URL.createObjectURL(file));
-    };
+    }
   };
   useEffect(() => {
-    if(imageFile){
+    if (imageFile) {
       uploadImage();
     }
   }, [imageFile]);
   const uploadImage = async () => {
-    setImageFileUploadError(null)
+    setImageFileUploading(true);
+    setImageFileUploadError(null);
     const storage = getStorage(app);
     const fileName = new Date().getTime() + imageFile.name;
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, imageFile);
     uploadTask.on(
-      'state_changed',
+      "state_changed",
       (snapshot) => {
         const progress =
-        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setImageFileUploadProgress(progress.toFixed(0));
       },
       (error) => {
-        setImageFileUploadError("Không thể đăng ảnh lên (Dung lượng ảnh không thể quá 5MB)");
+        setImageFileUploadError(
+          "Không thể đăng ảnh lên (Dung lượng ảnh không thể quá 5MB)"
+        );
         setImageFileUploadProgress(null);
         setImageFile(null);
         setImageFileUrl(null);
-        
+        setImageFileUploading(false);
       },
       () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl)=> {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
           setImageFileUrl(downloadUrl);
+          setFormData({ ...formData, profilePicture: downloadUrl });
+          setImageFileUploading(false);
         });
       }
-    )
+    );
   };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setUpdateUserError(null);
+    setUpdateUserSuccess(null);
+    if (Object.keys(formData).length === 0) {
+      setUpdateUserError('Không có gì thay đổi');
+      return;
+    }
+    if (imageFileUploading) {
+      setUpdateUserError('Vui lòng chờ ảnh được tải lên');
+      return;
+    }
+    try {
+      dispatch(updateStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(updateFailure(data.message));
+        setUpdateUserError(data.message);
+      } else {
+        dispatch(updateSuccess(data));
+        setUpdateUserSuccess("Thay đổi thông tin thành công!");
+      }
+    } catch (error) {
+      dispatch(updateFailure(error.message));
+      setUpdateUserError(error.message);
+    }
+  };
+
   return (
-    <div className='max-w-lg mx-auto p-3 w-full'>
-      <h1 className='my-7 text-center font-semibold text-3xl'>Hồ sơ</h1>
-      <form className='flex flex-col gap-4'>
-        <input type='file' accept='image/*' onChange={handleImageChange} ref={filePickerRef}/>
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">Hồ sơ</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
         <div
-          className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'
+          className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
           onClick={() => filePickerRef.current.click()}
         >
           {imageFileUploadProgress && (
@@ -68,9 +136,9 @@ export default function DashProfile() {
               strokeWidth={5}
               styles={{
                 root: {
-                  width: '100%',
-                  height: '100%',
-                  position: 'absolute',
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
                   top: 0,
                   left: 0,
                 },
@@ -84,51 +152,63 @@ export default function DashProfile() {
           )}
           <img
             src={imageFileUrl || currentUser.profilePicture}
-            alt='user'
+            alt="user"
             className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
               imageFileUploadProgress &&
               imageFileUploadProgress < 100 &&
-              'opacity-60'
+              "opacity-60"
             }`}
           />
         </div>
-        {imageFileUploadError && <Alert color='failure'>{imageFileUploadError}</Alert>}
-        
+        {imageFileUploadError && (
+          <Alert color="failure">{imageFileUploadError}</Alert>
+        )}
+
         <TextInput
-          type='text'
-          id='username'
-          placeholder='Tên tài khoản'
+          type="text"
+          id="username"
+          placeholder="Tên tài khoản"
           defaultValue={currentUser.username}
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <TextInput
-          type='email'
-          id='email'
-          placeholder='Email'
+          type="email"
+          id="email"
+          placeholder="Email"
           defaultValue={currentUser.email}
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <TextInput
-          type='text'
-          id='fullName'
-          placeholder='Họ và tên'
+          type="text"
+          id="fullName"
+          placeholder="Họ và tên"
           defaultValue={currentUser.fullName}
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <TextInput
-          type='password'
-          id='password'
-          placeholder='Mật khẩu'
-          // onChange={handleChange}
+          type="password"
+          id="password"
+          placeholder="Mật khẩu"
+          onChange={handleChange}
         />
-        <Button type='submit' gradientDuoTone='cyanToBlue' outline>
+        <Button type="submit" gradientDuoTone="cyanToBlue" outline>
           Cập nhật
         </Button>
       </form>
-      <div className='text-red-500 flex justify-between mt-5'>
-        <span className='cursor-pointer'>Xóa tài khoản</span>
-        <span className='cursor-pointer'>Đăng xuất</span>
+      <div className="text-red-500 flex justify-between mt-5">
+        <span className="cursor-pointer">Xóa tài khoản</span>
+        <span className="cursor-pointer">Đăng xuất</span>
       </div>
+      {updateUserSuccess && (
+        <Alert color='success' className="mt-5">
+          {updateUserSuccess}
+        </Alert>
+      )}
+      {updateUserError && (
+        <Alert color='failure' className="mt-5">
+          {updateUserError}
+        </Alert>
+      )}
     </div>
-  )
+  );
 }
